2057a191a9862b1ff7c02fbe7f5be10da26ec2f7
phuong1102k <dtp11020@gmail.com>
2023-10-12 23:22:06 +0700

    Update: Homepage CSS.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,40 @@
-$('.owl-carousel').owlCarousel({
-    loop:false,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+$('.daily-product-list').owlCarousel({
+    loop:true,
+    margin:24,
+    nav: true,
+    dots: false,
+    items: 1    
 })
+
+$('.best-product-list').owlCarousel({
+    loop:true,
+    margin: 24,
+    nav: true,
+    dots: false,
+    items: 4    
+})
+
+$('.type-list').owlCarousel({
+    loop:true,
+    margin: 24,
+    nav: true,
+    dots: false,
+    items: 4    
+})
+
+$('.blog-list').owlCarousel({
+    loop:true,
+    margin: 24,
+    nav: true,
+    dots: false,
+    items: 4    
+})
+
+$('.review-list').owlCarousel({
+    loop:true,
+    margin: 24,
+    nav: true,
+    dots: false,
+    items: 3   
+})
+
