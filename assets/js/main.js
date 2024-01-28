1bba33b320859a549b8de05deec070d9b8dc203d
phuong1102k <dtp11020@gmail.com>
2024-01-28 19:34:00 +0700

    :Project

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ const swiperEl = document.querySelector("swiper-container");
           },
           1024: {
             slidesPerView: 3,
-            spaceBetween: 50,
+            spaceBetween: 30,
           },
         },
       });
@@ -121,3 +121,89 @@ $('.more-list').owlCarousel({
     }   
 })
 
+// detail page
+// quantity
+var amountElement = document.getElementById("amount");
+var amount = parseInt(amountElement.value);
+
+let render = (amount) => {
+    amountElement.value = amount;
+    document.querySelector('.quantity-text').textContent = amount;
+}
+// plus
+let handlePlus = () => {
+    amount++;
+    render(amount);
+}
+// minus
+let handleMinus = () => {
+    if (amount > 1) amount--;
+    render(amount);
+}
+
+amountElement.addEventListener("input", () => {
+    amount = parseInt(amountElement.value); 
+    amount = (isNaN(amount) || amount == 0) ? 1 : amount;
+    render(amount);
+});
+
+// content product detail
+var content = document.getElementById("content");
+var button = document.getElementById("show_more");
+
+function toggleContent() {
+    if (content.classList.contains("open")) {
+        button.innerHTML = "Show more";
+        content.classList.remove("open");
+        button.classList.remove("open");
+    } else {
+    content.classList.add("open");
+    button.classList.add("open");
+    button.innerHTML = "<a>Show less</a>";
+    }
+}
+
+// tabs
+var text = document.getElementsByClassName("text");
+var more = document.getElementsByClassName("more");
+var btn = document.getElementsByClassName("tab-button");
+function toggleTab(i) {
+    if (text[i].classList.contains("open")) {
+        text[i].classList.remove("open");
+        more[i].classList.remove("open");
+    } else {
+        text[i].classList.add("open");
+        more[i].classList.add("open");
+    }
+}
+
+
+$('.mobile').owlCarousel({
+    loop:true,
+    margin: 24,
+    nav: false,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        }
+        
+    }   
+})
+
+$('.discover-grid').owlCarousel({
+    loop:true,
+    margin: 24,
+    nav: true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        740: {
+            items:3
+        }
+        
+    }   
+})
